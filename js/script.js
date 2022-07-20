3eecc6d0cd0e8124ad83277d36bcf6a955c10521
Eikcaj.1998 <jw220998@gmail.com>
2022-07-20 15:44:01 +0200

    esercizio teminato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+/* Esercizio di oggi: Vue Email List
+nome repo: vue-email-list
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di
+ una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti 
+generati.*/
 console.log('js ok',Vue)
+
+const app = new Vue(
+    {
+        el:root,
+    
+        data:{
+            
+            emailList:[]
+            
+        },
+
+        created(){
+
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        this.emailList.push(response.data.response);
+                    });
+            }
+        }
+
+    }
+)
